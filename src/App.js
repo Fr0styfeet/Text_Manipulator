b06f105668a9bdf75ffc6b4ac993cb89911cef82
Anisha Kumari Lal <96767716+Fr0styfeet@users.noreply.github.com>
2023-10-08 13:34:56 +0530

    mode changes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import './App.css';
 import React, { useState } from 'react'; //hook
 import Navbar from "./components/navbar";
 import TextForm from "./components/TextForm";
+// import Alert from './components/Alert';
 
 
 function App() {
   const [mode,setMode]=useState('light'); //dark mode?
+  // const [alert,setAlert]=useState(null);
+
+  // const handleAlert=(message,type)=>{
+  //     setAlert({
+  //       msg:message,
+  //       type:type
+  //     })
+  // }
 
   const toggleMode=()=>{
     if(mode==='dark'){
@@ -21,6 +30,7 @@ function App() {
   return (
     <>
     <Navbar title="Text Manipulator" mode={mode} toggleMode={toggleMode}/>
+    {/* <Alert alert="this is alert"/> */}
     <div className="container my-4">
     <TextForm mode={mode}/>
     </div>
